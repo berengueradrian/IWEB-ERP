6920e9c10beff76c04567d33e14d4f8bdf864d6d
Javier Rodríguez <jrj10@alu.ua.es>
2023-01-24 13:38:39 +0100

    Hotfix. Remove 'Add employee' from supervisor dashboard

--- resources/js/src/views/dashboard/DashboardSuper.vue
+++ resources/js/src/views/dashboard/DashboardSuper.vue
@@ -1,6 +1,6 @@
 <template>
   <section style="display:flex; flex-direction: column">
-    <v-btn to="/superole/nuevo" color="primary" class="mb-10" style="align-self:flex-end">Añadir empleado</v-btn>
+    <v-btn to="/superole/nuevo" color="primary" class="mb-10" v-if="$store.state.user.admin" style="align-self:flex-end">Añadir empleado</v-btn>
     <general-table class="mb-10" v-if="$store.state.user.supervisor === 1" title="Tu equipo" :supervisor="true"></general-table>
     <general-table title="Todos los empleados" :supervisor="false"></general-table>
   </section>
