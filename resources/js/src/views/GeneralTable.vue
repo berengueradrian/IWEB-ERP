bae30c607d4077dcc691183db65af25502ab7f8d
Javier Rodríguez <jrj10@alu.ua.es>
2023-01-24 14:37:16 +0100

    Redesign done

--- resources/js/src/views/GeneralTable.vue
+++ resources/js/src/views/GeneralTable.vue
@@ -55,9 +55,15 @@ export default {
     }
   },
   async created() {
-    await this.$store.dispatch('fetchEmpleados')
-    await this.$store.dispatch('fetchCategorias')
-    await this.$store.dispatch('fetchSupervisores')
+    if (this.$store.state.empleados.length === 0){
+      await this.$store.dispatch('fetchEmpleados')
+    }
+    if (this.$store.state.categorias.length === 0){
+      await this.$store.dispatch('fetchCategorias')
+    }
+    if (this.$store.state.supervisores.length === 0){
+      await this.$store.dispatch('fetchSupervisores')
+    }
     this.categories = this.$store.state.categorias.map(category => category.name)
     this.categories.push('Elige uno')
     this.employees = this.$store.state.empleados
