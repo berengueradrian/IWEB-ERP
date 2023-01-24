c0ed8b6ca09ccb86250a7e54eaf4ba6bb84b9e3d
berengueradrian <berengueragulloadrian@gmail.com>
2023-01-24 09:20:04 +0100

    Añadido todo funcionando con los cambios de Javi, falta comprobar el porque el archivo se sube mal

--- resources/views/application.blade.php
+++ resources/views/application.blade.php
@@ -7,10 +7,10 @@
   <meta name="viewport" content="width=device-width,initial-scale=1.0">
   <meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>Materio – Free Vuetify Vuejs Laravel Admin Template</title>
+  <title>RRHH ERP2</title>
 
   <!-- Favicon -->
-  <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+  <link rel="shortcut icon" href="{{ asset('icons8-sitio-web-de-reanudar-16.png') }}">
   <link rel="preconnect" href="https://fonts.gstatic.com" />
 
   <!-- Font -->
@@ -21,7 +21,7 @@
 
 <body>
   <noscript>
-    <strong>We're sorry but Materio - Vuetify Vuejs Admin Template doesn't work properly without JavaScript enabled.
+    <strong>We're sorry but RRHH ERP2 doesn't work properly without JavaScript enabled.
       Please enable it to continue.</strong>
   </noscript>
 
